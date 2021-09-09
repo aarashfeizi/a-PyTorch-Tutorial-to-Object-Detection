d391152c4f67fbe6e5a3cbb3ede819dcac2e3b00
Aarash Feizi <aarash95@gmail.com>
2021-09-09 12:24:03 -0400

    all images in file

--- detect.py
+++ detect.py
@@ -97,7 +97,12 @@ def detect(original_image, min_score, max_overlap, top_k, suppress=None):
 
 
 if __name__ == '__main__':
-    img_path = '/media/ssd/ssd data/VOC2007/JPEGImages/000001.jpg'
-    original_image = Image.open(img_path, mode='r')
-    original_image = original_image.convert('RGB')
-    detect(original_image, min_score=0.2, max_overlap=0.5, top_k=200).save(f'./model_annotated/.png')
+    folder_path = f'/home/mila/f/feiziaar/projects/a-PyTorch-Tutorial-to-Object-Detection/mine/real_images'
+    files = os.listdir(folder_path)
+    files = [f for f in files if f.endswith('png') or f.endswith('.jpeg')]
+    for f in files:
+        print(i)
+        img_path = os.path.join(folder_path, f)
+        original_image = Image.open(img_path, mode='r')
+        original_image = original_image.convert('RGB')
+        detect(original_image, min_score=0.2, max_overlap=0.5, top_k=200).save(f'./model_annotated/{f}')
