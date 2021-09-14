f6d63197cb1065aa66420581ca672af481a430a6
Aarash Feizi <aarash95@gmail.com>
2021-09-14 14:14:11 -0400

    make dataset with legends

--- mine/dataset_generation.py
+++ mine/dataset_generation.py
@@ -6,7 +6,8 @@ from PIL import Image
 from PIL import ImageDraw
 import pandas as pd
 
-foreground_org = Image.open("img_template/grid.png")
+grid_org = Image.open("img_template/grid.png")
+legends_org = Image.open("img_template/legends.png")
 background_org = Image.open("img_template/blank.png")
 
 background_size = 512
@@ -37,16 +38,40 @@ for split_dataset_size, split in zip(dataset_sizes, ['TRAIN', 'TEST']):
     for i in range(split_dataset_size):
         if i % 100 == 0:
             print(f'{i} / {split_dataset_size}')
-        grid_size = np.random.randint(200, 512)
-        division_factor = np.random.randint(1, 11)
-        recolored_foreground = Image.fromarray(np.array(foreground_org) // division_factor)
-        max_offset = background_size - grid_size
-        foreground = recolored_foreground.resize((grid_size, grid_size))
+        legend_size = np.random.randint(200, 512)
+        grid_size = int(legend_size * 0.9)
+        division_factor = np.random.randint(1, 5)
+        recolored_grid = Image.fromarray(np.array(grid_org) // division_factor)
+
+        legends = legends_org.resize((legend_size, legend_size))
+        grid = recolored_grid.resize((grid_size, grid_size))
+
+        rand_num_circles = np.random.randint(0, 12)
+        for _ in range(rand_num_circles):
+            random_x_circle = np.random.randint(0, grid_size)
+            random_y_circle = np.random.randint(0, grid_size)
+            random_circle_r = np.random.randint(grid_size // 40, grid_size // 30)
+            random_R = np.random.randint(0, 256)
+            random_G = np.random.randint(0, 256)
+            random_B = np.random.randint(0, 256)
+            draw_circles(grid,
+                         x=random_x_circle,
+                         y=random_y_circle,
+                         r=random_circle_r,
+                         color=(random_R, random_G, random_B, 255))
+
+        legends.paste(grid, (legend_size // 10, legend_size // 50), grid)
+
         background = background_org.resize((background_size, background_size))
+
+        max_offset = background_size - legend_size
         x_offset = np.random.randint(0, max_offset)
         y_offset = np.random.randint(0, max_offset)
-        background.paste(foreground, (x_offset, y_offset), foreground)
-        bbox = (x_offset, y_offset, x_offset + grid_size, y_offset + grid_size)
+        background.paste(legends, (x_offset, y_offset), legends)
+        bbox = (x_offset + legend_size // 10,
+                y_offset + legend_size // 50,
+                x_offset + legend_size // 10 + grid_size,
+                y_offset + legend_size // 50 + grid_size)
 
         dataset_info['img'] += [f'images_{split}/{i}.png']
 
@@ -56,19 +81,7 @@ for split_dataset_size, split in zip(dataset_sizes, ['TRAIN', 'TEST']):
         dataset_info['y_max'] += [bbox[3]]
 
         dataset_info['label'] += [20]
-        rand_num_circles = np.random.randint(0, 12)
-        for _ in range(rand_num_circles):
-            random_x_circle = np.random.randint(bbox[0], bbox[2])
-            random_y_circle = np.random.randint(bbox[1], bbox[3])
-            random_circle_r = np.random.randint(grid_size // 40, grid_size // 30)
-            random_R = np.random.randint(0, 256)
-            random_G = np.random.randint(0, 256)
-            random_B = np.random.randint(0, 256)
-            draw_circles(background,
-                         x=random_x_circle,
-                         y=random_y_circle,
-                         r=random_circle_r,
-                         color=(random_R, random_G, random_B, 255))
+
         background.save(f'./gen_dataset/images_{split}/{i}.png')
 
         draw_circles(background, bbox[2], bbox[3])
